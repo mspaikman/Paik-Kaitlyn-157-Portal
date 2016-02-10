76c92201fc39897ddaea4532b629f375155abac9
mspaikman <sd@gmail.com>
2016-02-09 20:28:55 -0800

    refresh

---
+++ js/ex1.js
@@ -0,0 +1,27 @@
+
+
+console.log("reading from js");
+
+
+function setup(){
+    var myCanvas=createCanvas(800,250);
+    myCanvas.parent("mySketch");
+    //link to id mySketch from header tag
+    myCanvas.parent("mySketch");
+    smooth();
+    noStroke ();
+    noCursor();
+
+}
+
+
+function draw(){
+  background(mouseX/2 + mouseY/2, 200, 200);
+  translate (width/2, height/2);}
+
+
+
+
+  
+
+  
